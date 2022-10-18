6b73942862ee37df7d1eed65fe930f0898a147ab
mostafabiabani <mostafabiabani1376@gmail.com>
2022-10-18 23:26:31 +0330

    chore(mostafa): initialize spring boot project

---
+++ src/main/java/com/radiojavan/downloader/RadioJavanDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.radiojavan.downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioJavanDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadioJavanDownloaderApplication.class, args);
+    }
+
+}
